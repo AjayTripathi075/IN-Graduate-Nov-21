c7f6c7ab7df3468148a75491e56cf6753a98d932
Ajay Tripathi <69579091+AjayTripathi075@users.noreply.github.com>
2021-12-13 17:05:59 +0530

    Assignments

---
+++ day06/demo04/js/app.js
@@ -0,0 +1,9 @@
+const mycity = {
+  City : "Pune",
+  popular:true,
+  Country : "India"
+};
+//mycity.City ="mumbai";
+//delete mycity.City;
+mycity.newCity ="Mumbai";
+console.log(mycity);
---
+++ day06/demo04/js/nested.js
@@ -0,0 +1,11 @@
+const mycity1 =
+{
+  City : "Pune",
+  Info : {
+    popular : true,
+    Country : "India"
+  }
+};
+
+console.log(mycity1);
+console.log(mycity1.Info.Country);
